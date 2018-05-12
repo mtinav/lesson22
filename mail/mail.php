928b1cddd23c6f2b9c071fd505b8e58d69aa1693
Unknown <MANTUSHKINAVA@GMAIL.COM>
2018-05-12 23:32:28 +0700

    asdzvf

--- mail/mail.php
+++ mail/mail.php
@@ -27,3 +27,11 @@ $headers = "MIME-Version: 1.0" . PHP_EOL .
 'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
 'Reply-To: '.$admin_email.'' . PHP_EOL;
 mail($admin_email, adopt($form_subject), $message, $headers );
+
+/**
+ * ssh valentina@karpov.today
+ * 
+ * cd source/project/public/lesson_2.22
+ * 
+ * 
+ */
